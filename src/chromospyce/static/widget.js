8105c75126545ff843db92d12dbc8c6b9d7b89ef
David Kouril <david.kouril@gmail.com>
2024-06-08 13:37:38 -0400

    chore: update to newer chromospace api

--- src/chromospyce/static/widget.js
+++ src/chromospyce/static/widget.js
@@ -1,5 +1,5 @@
 // @deno-types="npm:chromospace"
-import * as chs from "https://esm.sh/chromospace@0.0.8";
+import * as chs from "https://esm.sh/chromospace@0.0.10";
 //import * as chs from "http://localhost:5173/src/main.ts";
 
 /**
@@ -19,59 +19,61 @@ import * as chs from "https://esm.sh/chromospace@0.0.8";
 export default {
   /** @type {import("npm:@anywidget/types@0.1.6").Render<Model>} */
   render({ model, el }) {
+
     const options = {
       center: true,
       normalize: true,
     };
 
-    const renderer = new chs.ChromatinBasicRenderer();
-    const chromatinScene = {
-      chunks: [],
-      models: [],
-    };
+    //~ create a scene
+    let chromatinScene = chs.initScene();
+
+    //~ this config specifies how the 3D model will look
+    const viewConfig = ({ 
+      binSizeScale: 0.01, 
+      coloring: "constant",
+    });
 
     //~ process numpy
     const structureNumpy = model.get("structure_nparray");
     const isNumpy = (structureNumpy  != undefined && structureNumpy != null && structureNumpy.byteLength != 0);
     if (isNumpy) {
+      console.log("-----------------");
       console.log("numpy array was supplied!");
       const numpyChunk = chs.parseNumpyArray(structureNumpy , options);
-      chromatinScene.chunks.push(numpyChunk );
+      console.log(numpyChunk);
+      chromatinScene = chs.addChunkToScene(chromatinScene, numpyChunk, viewConfig);
     }
 
     //~ process string
     const structureString = model.get("structure_string");
     if (structureString != "") {
+      console.log("-----------------");
       console.log("string was supplied!");
       const stringChunk = chs.parseXYZ(
         structureString,
         model.get("delimiter"),
         options,
       );
-      chromatinScene.chunks.push(stringChunk);
+      chromatinScene = chs.addChunkToScene(chromatinScene, stringChunk, viewConfig);
     }
     
     //~ process filepath
     const structurePath = model.get("structure_path");
-    if (!structurePath) {
-      console.log("invalid path!");
-    }
     const isPath = (structurePath && structurePath.name != "");
     if (isPath) {
+      console.log("-----------------");
       console.log("path was supplied!");
       const pathChunk = chs.parseXYZ(
         structurePath.contents,
         model.get("delimiter"),
         options,
       );
-      chromatinScene.chunks.push(pathChunk );
+      chromatinScene = chs.addChunkToScene(chromatinScene, pathChunk, viewConfig);
     }
 
-    renderer.addScene(chromatinScene);
-    //~ start frame loop (requestAnimationFrame)
-    renderer.startDrawing();
-
-    el.appendChild(renderer.getCanvasElement());
+    const [renderer, canvas] = chs.display(chromatinScene, { alwaysRedraw: false});
+    el.appendChild(canvas);
 
     return () => {
       // Optionally cleanup
