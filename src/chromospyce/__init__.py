5acb158d2f4e747ee8bcbce06a3e020a7001e8f2
David Kouril <david.kouril@gmail.com>
2024-08-27 14:45:13 -0400

    feat: structure import only via Arrow

--- src/chromospyce/__init__.py
+++ src/chromospyce/__init__.py
@@ -9,32 +9,9 @@ try:
 except importlib.metadata.PackageNotFoundError:
     __version__ = "unknown"
 
-def text_file_serializer(path: pathlib.Path, widget):
-    if path is None:
-        return None
-
-    if path.exists():
-        if path.is_file():
-            return {
-                "name": path.name,
-                "contents": path.read_text()
-            }
-
 class Widget(anywidget.AnyWidget):
     _esm = pathlib.Path(__file__).parent / "static" / "widget.js"
     _css = pathlib.Path(__file__).parent / "static" / "widget.css"
 
-    # 3D structure input type: 1) path to file, 2) string, 3) numpy array
-    structure_path = traitlets.Instance(pathlib.Path, default_value=None, allow_none=True).tag(sync=True, to_json=text_file_serializer)
-    structure_string = traitlets.Unicode("").tag(sync=True)
-    structure_nparray = traitlets.Bytes().tag(sync=True)
-
-    # additional parameters
-    delimiter = traitlets.Unicode(" ").tag(sync=True)
-
-    def add_chunk_numpyarray():
-        print("not implemented")
-
-    def add_chunk_xyz():
-        print("not implemented")
-
+    # 3D structure input: assumes Apache Arrow format
+    structure = traitlets.Bytes().tag(sync=True)
--- src/chromospyce/static/widget.js
+++ src/chromospyce/static/widget.js
@@ -19,7 +19,6 @@ import * as chs from "http://localhost:5173/src/main.ts";
 export default {
   /** @type {import("npm:@anywidget/types@0.1.6").Render<Model>} */
   render({ model, el }) {
-
     const options = {
       center: true,
       normalize: true,
@@ -34,45 +33,14 @@ export default {
       color: "#ff00ff",
     };
 
-    //~ process numpy
-    const structureNumpy = model.get("structure_nparray");
-    const isNumpy = (structureNumpy  != undefined && structureNumpy != null && structureNumpy.byteLength != 0);
-    if (isNumpy) {
-      console.log("-----------------");
-      console.log("numpy array was supplied!");
-      const numpyChunk = chs.parseNumpyArray(structureNumpy , options);
-      console.log(numpyChunk);
-      chromatinScene = chs.addChunkToScene(chromatinScene, numpyChunk, viewConfig);
-    }
+    //~ process input
+    const structure = model.get("structure");
+    const chunk = chs.load(structure, options);
+    chromatinScene = chs.addChunkToScene(chromatinScene, chunk, viewConfig);
 
-    //~ process string
-    const structureString = model.get("structure_string");
-    if (structureString != "") {
-      console.log("-----------------");
-      console.log("string was supplied!");
-      const stringChunk = chs.parseXYZ(
-        structureString,
-        model.get("delimiter"),
-        options,
-      );
-      chromatinScene = chs.addChunkToScene(chromatinScene, stringChunk, viewConfig);
-    }
-    
-    //~ process filepath
-    const structurePath = model.get("structure_path");
-    const isPath = (structurePath && structurePath.name != "");
-    if (isPath) {
-      console.log("-----------------");
-      console.log("path was supplied!");
-      const pathChunk = chs.parseXYZ(
-        structurePath.contents,
-        model.get("delimiter"),
-        options,
-      );
-      chromatinScene = chs.addChunkToScene(chromatinScene, pathChunk, viewConfig);
-    }
-
-    const [renderer, canvas] = chs.display(chromatinScene, { alwaysRedraw: false});
+    const [renderer, canvas] = chs.display(chromatinScene, {
+      alwaysRedraw: false,
+    });
     el.appendChild(canvas);
 
     return () => {
