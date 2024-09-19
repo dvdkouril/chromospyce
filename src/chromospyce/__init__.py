efbdd71f6866ce52f0d0ac1105fed9fe6082e7fb
David Kouril <david.kouril@gmail.com>
2024-09-19 15:16:59 -0400

    sketch out the conversion function

--- src/chromospyce/__init__.py
+++ src/chromospyce/__init__.py
@@ -9,6 +9,25 @@ try:
 except importlib.metadata.PackageNotFoundError:
     __version__ = "unknown"
 
+
+def from_numpy(nparr):
+    """
+    This assumes `nparr` is a two-dimensional numpy array with xyz coordinates: [[x,y,z], ...]
+    """
+    xyz = nparr
+    # Convert numpy array to pandas dataframe
+    xyzDF = pd.DataFrame({'x': xyz[:, 0], 'y': xyz[:, 1], 'z': xyz[:, 2]})
+    # Convert pandas DF to Arrow Table
+    xyzArrowTable = pa.Table.from_pandas(xyzDF)
+    # Convert the Table to bytes
+    output_stream = pa.BufferOutputStream()
+    with pa.ipc.RecordBatchStreamWriter(output_stream, xyzArrowTable.schema) as writer:
+        writer.write_table(xyzArrowTable)
+
+    # Get the table as Bytes
+    table_bytes = output_stream.getvalue().to_pybytes()
+    return table_bytes
+
 class Widget(anywidget.AnyWidget):
     _esm = pathlib.Path(__file__).parent / "static" / "widget.js"
     _css = pathlib.Path(__file__).parent / "static" / "widget.css"
